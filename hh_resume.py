0cf357bffdb23a57d1cdafadd21bdea6539fe6cf
EvgenJin <rumbiroid@gmail.com>
2020-01-14 14:01:53 +0500

    Update hh_resume.py

--- hh_resume.py
+++ hh_resume.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas as pd
 
-file1 = open("myfile.txt","w") 
+# страница со всеми резюме 
 def get_all (page):
-  period = 7
-  area = 3
-  querry = 'Java'
-  url = 'https://hh.ru/search/resume?text='+querry +'&clusters=true&order_by=publication_time&no_magic=false&st=resumeSearch&logic=normal&pos=keywords&specialization=1&order_by=publication_time&exp_period=all_time&area=' + str(area) + '&search_period='+str(period)+'&page='+str(page)
+  period = 7 # за сколько дней
+  area = 1 # Екатеринбург 3 / Москва 1
+  querry = 'plsql' # Ключевое слово
+  url = 'https://hh.ru/search/resume?text='+querry +'&specialization=1&clusters=true&order_by=publication_time&no_magic=false&st=resumeSearch&logic=normal&pos=keywords&order_by=publication_time&exp_period=all_time&area=' + str(area) + '&search_period='+str(period)+'&page='+str(page)
+  #pos=full_text / keywords
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
   res = requests.get(url, headers=headers)
   soup_all = BeautifulSoup(res.content, 'html.parser')
@@ -15,40 +17,50 @@ def get_all (page):
   # print(res.status_code)
   return data_all
 
+# scan page with one vacancy
 def get_one(id):
   url = 'https://hh.ru'+id
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
   res = requests.get(url, headers=headers)
   return res
 
-# soup_all = BeautifulSoup(get_all(1).content, 'html.parser')
-# data_all = soup_all.find_all('div', {'data-qa':"resume-serp__resume"})
-# print(len(data_all))
-# for item in data_all:
-  # print(item.find('a', href=True)['href'])
+# if null function
+def if_null_bf(par):
+  if par:
+    res = par.get_text()
+  else:
+    res = ''  
+  return res
 
+# колонки и массив строк для dataframe
+columns = ['title','dbirth','salary','last_work','last_work_per','url']
+rows = []
 
 i=0
+# запрашивать пока массив не опустеет
 while len(get_all(i)) > 0:
   result_all = get_all(i)
-  # soup_all = BeautifulSoup(result_all.content, 'html.parser')
-  # data_all = soup_all.find_all('div', {'data-qa':"resume-serp__resume"})
-  i = i + 1
-  # print(result_all)
+  # i = i + 1
+  i +=1
   for item in result_all:
-    print(item.find('a', href=True)['href'])
-    # file1.write(item.find('a', href=True)['href'] + '\n') 
-# file1.close()  
-  #   id = item.find('a', href=True)['href']
-  #   result_one = get_one(id)
-  #   soup_one = BeautifulSoup(result_one.content, 'html.parser')
-  #   title = soup_one.find('span',class_='resume-block__title-text resume-block__title-text_position').find("span").get_text()
-  #   salary_tag = soup_one.find('span',class_='resume-block__salary resume-block__title-text_salary')
-  #   if salary_tag:
-  #     salary = salary_tag.get_text()
+    id = item.find('a', href=True)['href']
+    result_one = get_one(id)
+    soup_one = BeautifulSoup(result_one.content, 'html.parser')
+    title = soup_one.find('span',class_='resume-block__title-text resume-block__title-text_position').find("span")
+    dbirth = soup_one.find('meta',itemprop="birthDate")
+    if dbirth:
+      dbirth = dbirth["content"]
+    else:
+      dbirth = ''    
+    salary = soup_one.find('span',class_='resume-block__salary resume-block__title-text_salary')
+    last_work = soup_one.find('div',class_='resume-block__sub-title')
+    last_work_per = soup_one.find('div',class_='bloko-column bloko-column_xs-4 bloko-column_s-2 bloko-column_m-2 bloko-column_l-2')
+    row = [if_null_bf(title),dbirth,if_null_bf(salary),if_null_bf(last_work),if_null_bf(last_work_per),'https://hh.ru'+id]
+    rows.append(row)
 
-    # print(title + ' ' + salary)
-    # print(id)
+df = pd.DataFrame(rows, columns=columns)
+print(df[['title','dbirth','salary','last_work']])
+
 
 
 
